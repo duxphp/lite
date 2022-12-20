11f46245dd570f380c94ecae87e8130397881935
Mr.L <admin@duxphp.com>
2022-12-20 21:38:00 +0800

    update

---
+++ src/Handlers/ExceptionNotFound.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+namespace Dux\Handlers;
+
+use Throwable;
+
+/**
+ * ExceptionBusiness
+ */
+class ExceptionNotFound  extends Exception {
+
+    public function __construct() {
+        parent::__construct("Not Found", 404);
+    }
+
+}
