1fce5a5659fe1d1daf99d0b8b7740c3eef62e36e
mr.l <admin@duxphp.com>
2022-12-20 17:10:24 +0800

    update

--- src/Func/Response.php
+++ src/Func/Response.php
@@ -2,6 +2,13 @@
 
 use Psr\Http\Message\ResponseInterface;
 
+/**
+ * @param ResponseInterface $response
+ * @param string $message
+ * @param array $data
+ * @param int $code
+ * @return ResponseInterface
+ */
 function send(ResponseInterface $response, string $message, array $data = [], int $code = 200): ResponseInterface {
     $resfult = [];
     $resfult["code"] = $code;
@@ -11,4 +18,15 @@ function send(ResponseInterface $response, string $message, array $data = [], in
     $response->withHeader('Content-Type', 'application/json')->withStatus($code);
     $response->getBody()->write($payload);
     return $response;
+}
+
+
+/**
+ * @param string $message
+ * @param int $code
+ * @return mixed
+ * @throws Exception
+ */
+function error(string $message, int $code = 200): mixed {
+    throw new Exception($message, $code);
 }
