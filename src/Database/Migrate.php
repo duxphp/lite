dc0e91700e998b0395492f35b64b1a5656d9df09
mr.l <admin@duxphp.com>
2022-12-21 18:03:10 +0800

    update

--- src/Database/Migrate.php
+++ src/Database/Migrate.php
@@ -21,54 +21,117 @@ class Migrate {
         foreach ($this->migrate as $model) {
             $modelObject = new $model;
             $name = $modelObject->getTable();
-            $data = $this->db->query("SELECT COUNT(*) as num FROM information_schema.TABLES WHERE table_name = '$tableName'")->fetch();
-            $hasTable = (bool) $data["num"];
-            $struct = $modelObject->struct;
-            $rules = $this->migrateRule($struct);
+            $dataname = $this->db->getConfig("database");
+            $data = $this->db->query("SELECT COUNT(*) as num FROM information_schema.TABLES WHERE table_schema = '$dataname' AND table_name = '$name'")->fetch();
+            $hasTable = (bool)$data["num"];
+            $schema = $modelObject->getSchema();
+            $rules = $this->migrateRule($schema);
             if (!$hasTable) {
                 // 创建表
                 $fieldArray = [];
-                $afterFiled = "";
-                $i = 0;
                 foreach ($rules as $field => $rule) {
-                    $row = [];
+                    $row = [$field];
+                    $i = 0;
                     foreach ($rule as $method => $params) {
                         switch ($method) {
                             case "primarykey":
-                                $row[] = "$method unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY";
+                                $row[] = "unsigned AUTO_INCREMENT PRIMARY KEY NOT NULL ";
+                                break;
                             case "comment":
                             case "default":
-                            $row[] = "$method '$params'";
+                                $row[] = "$method '$params'";
+                                break;
                             case "null":
                                 $row[] = "DEFAULT NULL";
+                                break;
                             case "notnull":
                                 $row[] = "NOT NULL";
+                                break;
                             default:
-                                if ($i == 0 && $params) {
-                                    $method = "$method($params)";
+                                if ($i == 0) {
+                                    if ($params) {
+                                        $method = "$method($params)";
+                                    }
                                 }
                                 $row[] = $method;
                         }
-                        if ($afterFiled) {
-                            $row[] = "after " . $afterFiled;
-                        }
+                        $i++;
                     }
-                    $afterFiled = $field;
                     $fieldArray[] = implode(" ", $row);
-                    $i++;
                 }
                 $fieldSql = implode(",", $fieldArray);
-
-                $this->db->query("create table $tableName ($fieldSql)");
+                $this->db->query("create table $name ($fieldSql)");
             } else {
-                // 更新表
+                $result = $this->db->query("select count(*) as num
+                        from information_schema.columns
+                        where table_schema = '$dataname'
+                        and table_name = '$name'
+                        and column_name = '$field'")->fetch();
+                $hasColumn = (bool)$result["num"];
+
+                // 更新字段
+                $lastField = "";
+                foreach ($rules as $field => $rule) {
+                    $row = [$field];
+                    $i = 0;
+
+                    if ($lastField) {
+                        $row[] = "after $lastField";
+                    }
+                    $sql = implode(" ", $row);
+
+
+                    if ($hasColumn) {
+                        //修改字段
+                        $this->db->query("alter table $name modify column $sql");
+                    } else {
+                        //新增字段
+                        $this->db->query("alter table $name add column $sql");
+
+
+                    }
+                    $lastField = $field;
+                }
 
 
             }
 
         }
+    }
 
-
+    private function generateCol(array $rule, bool $update = false): array {
+        $i = 0;
+        foreach ($rule as $method => $params) {
+            switch ($method) {
+                case "primarykey":
+                    if (!$update) {
+                        $row[] = "unsigned AUTO_INCREMENT PRIMARY KEY NOT NULL ";
+                    }
+                    break;
+                case "comment":
+                case "default":
+                    $row[] = "$method '$params'";
+                    break;
+                case "null":
+                    $row[] = "DEFAULT NULL";
+                    break;
+                case "notnull":
+                    $row[] = "NOT NULL";
+                    break;
+                case "unsigned":
+                    $row[] = "unsigned";
+                    break;
+                default:
+                    if ($i == 0) {
+                        if ($params) {
+                            $method = "$method($params)";
+                        }
+                    }
+                    $row[] = $method;
+            }
+            $i++;
+        }
+        return $row;
     }
 
     private function migrateRule(array $struct): array {
