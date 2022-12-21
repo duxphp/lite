1f3a91387f1d49eba85e241dfa8e6912b02672ad
mr.l <admin@duxphp.com>
2022-12-21 16:54:44 +0800

    update

--- src/Database/Migrate.php
+++ src/Database/Migrate.php
@@ -21,64 +21,54 @@ class Migrate {
         foreach ($this->migrate as $model) {
             $modelObject = new $model;
             $name = $modelObject->getTable();
-            //$pre = $this->db->getConfig("prefix");
-
-            $data = $this->db->query("SELECT COUNT(*) FROM information_schema.TABLES WHERE table_name = <$name>")->fetchAll();
-
+            $data = $this->db->query("SELECT COUNT(*) as num FROM information_schema.TABLES WHERE table_name = '$tableName'")->fetch();
+            $hasTable = (bool) $data["num"];
             $struct = $modelObject->struct;
             $rules = $this->migrateRule($struct);
-            if (!$this->capsule->schema()->hasTable($name)) {
+            if (!$hasTable) {
                 // 创建表
-                $this->capsule->schema()->create($name, function (Blueprint $table) use ($rules) {
-                    foreach ($rules as $field => $rule) {
-                        $this->createField($table, $field, $rule);
+                $fieldArray = [];
+                $afterFiled = "";
+                $i = 0;
+                foreach ($rules as $field => $rule) {
+                    $row = [];
+                    foreach ($rule as $method => $params) {
+                        switch ($method) {
+                            case "primarykey":
+                                $row[] = "$method unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY";
+                            case "comment":
+                            case "default":
+                            $row[] = "$method '$params'";
+                            case "null":
+                                $row[] = "DEFAULT NULL";
+                            case "notnull":
+                                $row[] = "NOT NULL";
+                            default:
+                                if ($i == 0 && $params) {
+                                    $method = "$method($params)";
+                                }
+                                $row[] = $method;
+                        }
+                        if ($afterFiled) {
+                            $row[] = "after " . $afterFiled;
+                        }
                     }
-                });
+                    $afterFiled = $field;
+                    $fieldArray[] = implode(" ", $row);
+                    $i++;
+                }
+                $fieldSql = implode(",", $fieldArray);
+
+                $this->db->query("create table $tableName ($fieldSql)");
             } else {
                 // 更新表
-                $this->capsule->schema()->table($name, function (Blueprint $table) use ($pre, $name, $rules) {
-                    $table->string('name', 50)->change();
-                    foreach ($rules as $field => $rule) {
-                        // 创建字段
-                        if (!$this->capsule->schema()->hasColumn($name, $field)) {
-                            $this->createField($table, $field, $rule);
-                            continue;
-                        }
-                        // 跳过无法更新字段
-                        if (!in_array($this->capsule->schema()->getColumnType($name, $field), $this->updateFields)) {
-                            continue;
-                        }
-                        // 更新字段
-                        $this->createField($table, $field, $rule)->change();
-                    }
-                });
+
 
             }
 
         }
 
 
-    }
-
-    private function createField(Blueprint $table, string $field, array $rule): ColumnDefinition {
-        // 创建字段
-        $type = array_key_first($rule);
-        $fieldObject = $table->$type($field, ...$rule[$type]);
-        // 设置字段
-        $i = 0;
-        foreach ($rule as $method => $params) {
-            if ($i == 0) {
-                continue;
-            }
-            if ($params) {
-                $fieldObject = call_user_func($fieldObject, $method, ...$params);
-            } else {
-                $fieldObject = call_user_func($fieldObject, $method);
-            }
-
-            $i++;
-        }
-        return $fieldObject;
     }
 
     private function migrateRule(array $struct): array {
@@ -90,7 +80,7 @@ class Migrate {
                 $tmp = explode(':', $vo, 2);
                 $method = $tmp[0];
                 $params = isset($tmp[1]) ? $tmp[1] : "";
-                $ruleMaps[$method] = explode(',', $params);
+                $ruleMaps[$method] = $params;
             }
             $map[$field] = $ruleMaps;
         }
