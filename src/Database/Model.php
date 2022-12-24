bee5ecf41a747f8362ab773c8abc79a367af2fd7
Mr.L <admin@duxphp.com>
2022-12-24 11:57:51 +0800

    update

--- src/Database/Model.php
+++ src/Database/Model.php
@@ -8,18 +8,15 @@ use Exception;
 
 class Model extends \Illuminate\Database\Eloquent\Model
 {
-
-    protected array $schema = [];
-
-    const CREATED_AT = 'created_time';
-    const UPDATED_AT = 'updated_time';
+    const CREATED_AT = 'create_time';
+    const UPDATED_AT = 'update_time';
+    const DELETED_AT = 'delete_time';
+    protected $dateFormat = 'U';
     protected $fillable = [];
     protected $guarded = [];
-
-
+    protected array $schema = [];
     public function getSchema(): array {
         return $this->schema;
     }
-
 
 }
