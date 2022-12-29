9e2832b0cfe3c1c30894c17cf48c6f7e1f5eaa81
duxphp <admin@duxphp.com>
2022-12-29 19:16:47 +0800

    update

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -45,7 +45,7 @@ class Bootstrap {
      * init
      */
     public function __construct() {
-        error_reporting(E_ALL ^ E_NOTICE ^ E_DEPRECATED);
+        error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED);
     }
 
     public function loadFunc() {
