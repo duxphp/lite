cc98a551f09114832129504185977555439cb493
mr.l <admin@duxphp.com>
2022-12-29 17:35:26 +0800

    update

--- src/Database/Model.php
+++ src/Database/Model.php
@@ -8,10 +8,6 @@ use Exception;
 
 class Model extends \Illuminate\Database\Eloquent\Model
 {
-    const CREATED_AT = 'create_time';
-    const UPDATED_AT = 'update_time';
-    const DELETED_AT = 'delete_time';
-    protected $dateFormat = 'U';
     protected $fillable = [];
     protected $guarded = [];
     protected array $schema = [];
