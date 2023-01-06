a79da7817642301041465ee5a60f8147e92790db
mr.l <admin@duxphp.com>
2023-01-06 16:10:15 +0800

    update

--- src/Database/TreeTrait.php
+++ src/Database/TreeTrait.php
@@ -3,12 +3,25 @@
 namespace Dux\Database;
 
 trait TreeTrait {
-    public static function tree() {
+    public static function tree(): \BlueM\Tree {
         $data = self::query()->get();
         return new \BlueM\Tree($data->toArray(), ['rootId' => 0, 'id' => 'id', 'parent' => 'parent_id']);
     }
 
+    public static function childrenAll(int $parentId = 0) {
+        $model = new self();
+        $fields = "*";
+        if (isset($model->treeFields)) {
+            $fields = $model->treeFields;
+        }
+        return $model->where("parent_id", 0)->with(['children'])->select($fields)->get();
+    }
+
     public function children() {
-        return $this->hasMany(get_class($this), 'parent_id' ,'id')->with(['children']);
+        $fields = "*";
+        if (isset($this->treeFields)) {
+            $fields = $this->treeFields;
+        }
+        return $this->hasMany(get_class($this), 'parent_id' ,'id')->select($fields)->with(['children']);
     }
 }
