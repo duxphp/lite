0f57ca5e0481a79e88d563699cc8c111208c1872
mr.l <admin@duxphp.com>
2023-01-13 15:13:10 +0800

    update

--- src/Func/Common.php
+++ src/Func/Common.php
@@ -2,6 +2,8 @@
 declare(strict_types=1);
 
 use Carbon\Carbon;
+use JetBrains\PhpStorm\NoReturn;
+use Symfony\Component\VarDumper\VarDumper;
 
 if (! function_exists('now')) {
     function now(): Carbon {
@@ -10,13 +12,19 @@ if (! function_exists('now')) {
 }
 
 if (! function_exists('dux_debug')) {
-    function dux_debug(...$args) {
-        header('Access-Control-Allow-Origin: *');
-        header('Access-Control-Allow-Methods: *');
-        header('Access-Control-Allow-Headers: *');
-        http_response_code(500);
-        dump(...$args);
-        die(1);
+    function dux_debug(...$args): void {
+
+        if (!in_array(\PHP_SAPI, ['cli', 'phpdbg'], true) && !headers_sent()) {
+            header('Access-Control-Allow-Origin: *');
+            header('Access-Control-Allow-Methods: *');
+            header('Access-Control-Allow-Headers: *');
+            http_response_code(500);
+        }
+
+        foreach ($args as $v) {
+            VarDumper::dump($v);
+        }
+        exit(1);
     }
 }
 
