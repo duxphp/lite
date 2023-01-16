0dbe55fdfdce38ab7924871449b1c55ef10a70d7
duxphp <admin@duxphp.com>
2023-01-16 23:59:04 +0800

    Changes

--- src/Auth/Auth.php
+++ src/Auth/Auth.php
@@ -11,35 +11,6 @@ use Slim\Routing\RouteContext;
 
 class Auth {
 
-    static public function middleware(string $app, int $renewal = 43200): \Tuupola\Middleware\JwtAuthentication {
-        $secret = \Dux\App::config("app")->get("app.secret");
-        return new \Tuupola\Middleware\JwtAuthentication([
-            "secret" => $secret,
-            "secure" => false,
-            "before" => function ($request, $arguments) {
-                $token = $arguments["decoded"];
-                return $request->withAttribute('auth', $token);
-            },
-            "after" => function ($response, $arguments) use ($renewal, $secret, $app) {
-                $token = $arguments["decoded"];
-                if ($app != $token["sub"]) {
-                    throw new \Dux\Handlers\ExceptionBusiness("Authorization app error", 401);
-                }
-                $renewalTime = $token["iat"] + $renewal;
-                $time = time();
-                if ($renewalTime <= $time) {
-                    $token["exp"] = $time + $token["exp"] - $token["iat"];
-                    $auth = JWT::encode($token, $secret);
-                    return $response->withHeader("Authorization", "Bearer $auth");
-                }
-                return $response;
-            },
-            "error" => function ($response, $arguments) {
-                throw new \Dux\Handlers\ExceptionBusiness($arguments["message"], 401);
-            }
-        ]);
-    }
-
     static public function token(string $app, $params = [], int $expire = 86400): string {
         $time = time();
         $payload = [
---
+++ src/Auth/AuthMiddleware.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Dux\Auth;
+
+use Firebase\JWT\JWT;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
+
+class AuthMiddleware {
+    public function __construct(
+        public string $app,
+        public int $renewal = 43200
+    ) {
+    }
+
+    public function __invoke(Request $request, RequestHandler $handler): Response {
+        $secret = \Dux\App::config("app")->get("app.secret");
+        $renewal = $this->renewal;
+        $app = $this->app;
+        $jwt = new \Tuupola\Middleware\JwtAuthentication([
+            "secret" => $secret,
+            "secure" => false,
+            "before" => function ($request, $arguments) {
+                $token = $arguments["decoded"];
+                return $request->withAttribute('auth', $token);
+            },
+            "after" => function ($response, $arguments) use ($renewal, $secret, $app) {
+                $token = $arguments["decoded"];
+                if ($app != $token["sub"]) {
+                    throw new \Dux\Handlers\ExceptionBusiness("Authorization app error", 401);
+                }
+                $renewalTime = $token["iat"] + $renewal;
+                $time = time();
+                if ($renewalTime <= $time) {
+                    $token["exp"] = $time + $token["exp"] - $token["iat"];
+                    $auth = JWT::encode($token, $secret);
+                    return $response->withHeader("Authorization", "Bearer $auth");
+                }
+                return $response;
+            },
+            "error" => function ($response, $arguments) {
+                throw new \Dux\Handlers\ExceptionBusiness($arguments["message"], 401);
+            }
+        ]);
+        return $jwt->process($request, $handler);
+    }
+}
