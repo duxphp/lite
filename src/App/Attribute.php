9f1930928c9d76488197c08afa51776f7fb19d8a
duxphp <admin@duxphp.com>
2023-01-17 10:13:20 +0800

    Changes

---
+++ src/App/Attribute.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace Dux\App;
+
+use Dux\App;
+
+class Attribute {
+
+    public static function load(): void {
+
+        foreach (App::$registerApp as $vo) {
+
+        }
+
+    }
+}
