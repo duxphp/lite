45e50d850e5c38f8d17614da100e59703e6722ca
duxphp <admin@duxphp.com>
2023-01-17 11:08:26 +0800

    Changes

--- src/App/Attribute.php
+++ src/App/Attribute.php
@@ -1,17 +1,54 @@
 <?php
-
+declare(strict_types=1);
 
 namespace Dux\App;
 
+use Composer\Autoload\ClassLoader;
 use Dux\App;
+use Nette\Utils\FileSystem;
+use Nette\Utils\Finder;
 
 class Attribute {
 
-    public static function load(): void {
-
-        foreach (App::$registerApp as $vo) {
-
+    static function load(array $apps): array {
+        $status = App::config("app")->get("cache", false);
+        $cachePath = data_path("/cache/app/attribute.json");
+        if (!$status) {
+            return self::get($apps);
         }
+        if (is_file($cachePath)) {
+            $content = FileSystem::read($cachePath);
+            return json_decode($content, true);
+        }
+        $data = self::get($apps);
+        FileSystem::write($cachePath, json_encode($data, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
+        return $data;
+    }
 
+    static function get($apps): array {
+        $data = [];
+        foreach ($apps as $vo) {
+            $reflection = new \ReflectionClass($vo);
+            $appDir = dirname($reflection->getFileName());
+            $files = Finder::findFiles("*/*.php")->from($appDir);
+            foreach ($files as $file) {
+                $class = $reflection->getNamespaceName() . "\\" . basename($file->getPath()) . "\\" . $file->getBasename(".php");
+                if (!class_exists($class)) {
+                    continue;
+                }
+                $reflection = new \ReflectionClass($class);
+                $attributes = $reflection->getAttributes();
+                foreach ($attributes as $attribute) {
+                    if (!isset($data[$attribute->getName()]) && !class_exists($attribute->getName())) {
+                        continue;
+                    }
+                    $data[$attribute->getName()][] = [
+                        "class" => $class,
+                        "params" => $attribute->getArguments()
+                    ];
+                }
+            }
+        }
+        return $data;
     }
 }
