c882badd77485fce3e5e39dfcbcb6f22bfa93dfa
mr.l <admin@duxphp.com>
2023-01-29 17:38:07 +0800

    update

---
+++ src/App/AppInstallCommand.php
@@ -0,0 +1,40 @@
+<?php
+declare(strict_types=1);
+
+namespace Dux\App;
+
+use Dux\App;
+use Nette\Utils\FileSystem;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\Table;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class AppInstallCommand extends Command {
+
+    protected static $defaultName = 'app:install';
+    protected static $defaultDescription = 'install applications in the system';
+
+
+    protected function configure(): void {
+        $this->addArgument(
+            'name',
+            InputArgument::REQUIRED,
+            'please enter the package name'
+        );
+    }
+
+    public function execute(InputInterface $input, OutputInterface $output): int {
+
+        $name = $input->getArgument('name');
+        $name = ucfirst($name);
+        $dir = base_path("/vendor/$name/src");
+        if (is_dir($dir)) {
+            return $this->error($output, 'The application already exists');
+        }
+        FileSystem::copy(base_path('/vendor/'), $dir);
+        return Command::SUCCESS;
+    }
+
+}
--- src/Composer/Installer.php
+++ src/Composer/Installer.php
@@ -2,16 +2,78 @@
 
 namespace Dux\Composer;
 
+use Composer\Installer\BinaryInstaller;
+use Composer\IO\IOInterface;
 use Composer\Package\PackageInterface;
 use Composer\Installer\LibraryInstaller;
+use Composer\PartialComposer;
+use Composer\Repository\InstalledRepositoryInterface;
+use Composer\Util\Filesystem;
+use Dux\App;
+use React\Promise\PromiseInterface;
 
 class Installer extends LibraryInstaller
 {
-    protected $type = 'dux-app';
+    private $process;
 
-    public function getInstallPath(PackageInterface $package) {
-        list($vendor, $dir) = explode('/', $package->getName());
-        return parent::getInstallPath($package);
+    public function __construct(IOInterface $io, PartialComposer $composer, ?string $type = 'library', ?Filesystem $filesystem = null, ?BinaryInstaller $binaryInstaller = null) {
+        parent::__construct($io, $composer, $type, $filesystem, $binaryInstaller);
+        $this->process = new ProcessExecutor($io);
     }
 
+    public function install(InstalledRepositoryInterface $repo, PackageInterface $package)
+    {
+        $this->initializeVendorDir();
+        $downloadPath = $this->getInstallPath($package);
+
+        // remove the binaries if it appears the package files are missing
+        if (!Filesystem::isReadable($downloadPath) && $repo->hasPackage($package)) {
+            $this->binaryInstaller->removeBinaries($package);
+        }
+
+        $promise = $this->installCode($package);
+        if (!$promise instanceof PromiseInterface) {
+            $promise = \React\Promise\resolve(null);
+        }
+
+        $binaryInstaller = $this->binaryInstaller;
+        $installPath = $this->getInstallPath($package);
+
+        return $promise->then(static function () use ($binaryInstaller, $installPath, $package, $repo): void {
+            $binaryInstaller->installBinaries($package, $installPath);
+            if (!$repo->hasPackage($package)) {
+                $repo->addPackage(clone $package);
+                $this->process->execute('php dux app:install ' . $package->getName());
+            }
+
+        });
+    }
+
+    public function supports($packageType)
+    {
+        return $packageType === 'dux-app';
+    }
+
+    public function copyDir($source, $destination, $child = 1)
+    {
+        if(!is_dir($source)){
+            return 0;
+        }
+        if(!is_dir($destination)){
+            mkdir($destination,0777);
+        }
+        $handle= dir($source);
+        while($entry = $handle->read()) {
+            if(($entry!=".")&&($entry!="..")){
+                if(is_dir($source."/".$entry)){
+                    if($child){
+                        $this -> xCopy($source."/".$entry,$destination."/".$entry,$child);
+                    }
+                }else{
+                    copy($source."/".$entry,$destination."/".$entry);
+                }
+            }
+        }
+        return 1;
+    }
 }
