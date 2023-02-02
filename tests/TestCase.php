1e8501056cc123f886a475f1b22ca8fbc2372d11
mr.l <admin@duxphp.com>
2023-02-02 14:29:18 +0800

    update

--- tests/TestCase.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Dux\tests;
-
-class TestCase extends \PHPUnit\Framework\TestCase{
-
-    public function setUp(): void {
-        \Dux\App::createCli(__DIR__ .'/../../..');
-    }
-}
