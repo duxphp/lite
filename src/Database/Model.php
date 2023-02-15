0b66de3d143b2988294f24fa1eed535445604963
duxphp <admin@duxphp.com>
2023-02-15 14:21:11 +0800

    update

--- src/Database/Model.php
+++ src/Database/Model.php
@@ -5,14 +5,26 @@ namespace Dux\Database;
 
 use Dux\App;
 use Exception;
+use Illuminate\Database\Connection;
+use Illuminate\Database\Schema\Blueprint;
 
 class Model extends \Illuminate\Database\Eloquent\Model
 {
     protected $fillable = [];
     protected $guarded = [];
     protected array $schema = [];
-    public function getSchema(): array {
+
+    public function getSchema(): array
+    {
         return $this->schema;
+    }
+
+    public function migration(Blueprint $table)
+    {
+    }
+
+    public function seed(Connection $db)
+    {
     }
 
 }
