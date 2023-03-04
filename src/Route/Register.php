5d060f536773447066864b586f5e7fed410921ba
duxphp <admin@duxphp.com>
2023-03-04 16:03:12 +0800

    更换注解参数

--- src/Route/Register.php
+++ src/Route/Register.php
@@ -8,7 +8,8 @@ use Dux\Handlers\Exception;
 use Dux\Route\Attribute\RouteGroup;
 use Dux\Route\Attribute\RouteManage;
 
-class Register {
+class Register
+{
 
     public array $app = [];
     public array $path = [];
@@ -19,7 +20,8 @@ class Register {
      * @param Route $route
      * @return void
      */
-    public function set(string $name, Route $route): void {
+    public function set(string $name, Route $route): void
+    {
         $this->app[$name] = $route;
     }
 
@@ -28,7 +30,8 @@ class Register {
      * @param string $name
      * @return Route
      */
-    public function get(string $name): Route {
+    public function get(string $name): Route
+    {
 
         if (!isset($this->app[$name])) {
             throw new Exception("The routing app [$name] is not registered");
@@ -40,13 +43,42 @@ class Register {
      * 注解路由注册
      * @return void
      */
-    public function registerAttribute(): void {
+    public function registerAttribute(): void
+    {
         $groupClass = [];
-        $attributes = (array) App::di()->get("attributes");
+        $attributes = (array)App::di()->get("attributes");
         foreach ($attributes as $attribute => $list) {
             if (
                 $attribute != RouteManage::class &&
-                $attribute != RouteGroup::class &&
+                $attribute != RouteGroup::class
+            ) {
+                continue;
+            }
+            foreach ($list as $vo) {
+                $params = $vo["params"];
+                $class = $vo["class"];
+                [$className, $methodName, $name] = $this->formatFile($class);
+                // group
+                if ($attribute == RouteGroup::class) {
+                    $group = $this->get($params["app"])->group($params["pattern"], $params["title"], ...($params["middleware"] ?? []));
+                    $groupClass[$className] = $group;
+                }
+                // manage
+                if ($attribute == RouteManage::class) {
+                    $group = $this->get($params["app"])->manage(
+                        pattern: $params["pattern"],
+                        class: $class,
+                        name: $params["name"] ?: $name,
+                        title: $params["title"],
+                        ways: $params["ways"] ?? []
+                    );
+                    $groupClass[$className] = $group;
+                }
+            }
+        }
+
+        foreach ($attributes as $attribute => $list) {
+            if (
                 $attribute != \Dux\Route\Attribute\Route::class
             ) {
                 continue;
@@ -54,51 +86,40 @@ class Register {
             foreach ($list as $vo) {
                 $params = $vo["params"];
                 $class = $vo["class"];
-                [$className, $methodName] = explode(":", $class, 2);
-                $classArr = explode("\\", $className);
-                $layout = array_slice($classArr, -3, 1)[0];
-                $name = lcfirst($layout) . "." . lcfirst(end($classArr));
-                // group
-                if ($attribute == RouteGroup::class) {
-                    $group = $this->get($params["app"])->group($params["pattern"], $params["title"], ...($params["middleware"] ?? []));
-                    $groupClass[$class] = $group;
-                }
-                // manage
-                if ($attribute == RouteManage::class) {
-                    $this->get($params["app"])->manage(
-                        pattern: $params["pattern"],
-                        class: $class,
-                        name: $params["name"] ?: $name,
-                        title: $params["title"],
-                        ways: $params["ways"] ?? []
-                    );
-                }
+                [$className, $methodName, $name] = $this->formatFile($class);
                 // route
-                if ($attribute == \Dux\Route\Attribute\Route::class) {
-                    if (str_contains($class, ":")) {
-                        // method
-                        if (!$params["app"] && !isset($groupClass[$className])) {
-                            throw new \Exception("class [" . $class . "] route attribute parameter missing \"app\" ");
-                        }
-                        $group = $params["app"] ? $this->get($params["app"]) : $groupClass[$className];
-                    } else {
-                        // class
-                        if (empty($params["app"])) {
-                            throw new \Exception("class [" . $class . "] route attribute parameter missing \"app\" ");
-                        }
-                        $group = $this->get($params["app"]);
+                if (str_contains($class, ":")) {
+                    // method
+                    if (!$params["app"] && !isset($groupClass[$className])) {
+                        throw new \Exception("class [" . $class . "] route attribute parameter missing \"app\" ");
                     }
-                    $group->map(
-                        methods: is_array($params["methods"]) ? $params["methods"] : [$params["methods"]],
-                        pattern: $params["pattern"] ?: '',
-                        callable: $class,
-                        name: $params["name"] ?: $name . ($methodName ? "." . lcfirst($methodName) : ""),
-                        title: $group->title . $params["title"]
-                    );
+                    $group = $params["app"] ? $this->get($params["app"]) : $groupClass[$className];
+                } else {
+                    // class
+                    if (empty($params["app"])) {
+                        throw new \Exception("class [" . $class . "] route attribute parameter missing \"app\" ");
+                    }
+                    $group = $this->get($params["app"]);
                 }
+                $group->map(
+                    methods: is_array($params["methods"]) ? $params["methods"] : [$params["methods"]],
+                    pattern: $params["pattern"] ?: '',
+                    callable: $class,
+                    name: $params["name"] ?: $name . ($methodName ? "." . lcfirst($methodName) : ""),
+                    title: $group->title . $params["title"]
+                );
             }
         }
+    }
 
+    private function formatFile($class): array
+    {
+        [$className, $methodName] = explode(":", $class, 2);
+        $classArr = explode("\\", $className);
+        $layout = array_slice($classArr, -3, 1)[0];
+        $name = lcfirst($layout) . "." . lcfirst(end($classArr));
+
+        return [$className, $methodName, $name];
     }
 
 }
--- src/Validator/Validator.php
+++ src/Validator/Validator.php
@@ -6,14 +6,16 @@ namespace Dux\Validator;
 // https://github.com/vlucas/valitron
 use Dux\Handlers\ExceptionValidator;
 
-class Validator {
+class Validator
+{
 
     /**
      * 数据验证
      * @param array $data data array
      * @param array $rules ["name" => ["rule", "message"]]
      */
-    public static function parser(array $data, array $rules): Data {
+    public static function parser(array $data, array $rules): Data
+    {
 //        $role = [
 //            "name" => ["rule", "message"]
 //        ];
@@ -22,13 +24,20 @@ class Validator {
             if (empty($item)) {
                 continue;
             }
+            if (!is_array($item[0])) {
+                $datas = [$item];
+            } else {
+                $datas = $item;
+            }
             $keys = explode('#', $key);
             $key = trim($keys[0]);
-            $message = last($item);
-            $params = array_slice($item, 1, -1);
-            $v->rule($item[0], $key, ...$params)->message($message);
+            foreach ($datas as $vo) {
+                $message = last($vo);
+                $params = array_slice($vo, 1, -1);
+                $v->rule($vo[0], $key, ...$params)->message($message);
+            }
         }
-        if(!$v->validate()) {
+        if (!$v->validate()) {
             throw new ExceptionValidator($v->errors());
         }
         $dataObj = new Data();
