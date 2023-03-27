4c0748fa2aebb86c9dd1d37f285b533b4bb11c1a
duxphp <admin@duxphp.com>
2023-03-27 18:20:26 +0800

    update

---
+++ src/Vaptcha/Vaptcha.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Dux\Vaptcha;
+
+use Dux\App;
+use Dux\Handlers\Exception;
+use GuzzleHttp\Client;
+
+class Vaptcha
+{
+    static function Verify(string $server, string $token)
+    {
+        $id = App::config('client')->get('vaptcha');
+        $key = App::config('client')->get('vaptcha_key');
+        $client = new Client();
+        $result = $client->post($server, [
+            'json' => [
+                'id' => $id,
+                'secretkey' => $key,
+                'scene' => 0,
+                'token' => $token,
+                'ip' => get_ip()
+            ]
+        ]);
+        if ($result->getStatusCode() != 200) {
+            throw new Exception('验证服务器失败，请重试');
+        }
+        $data = json_decode($result->getBody(), true);
+        if (!$data['success']) {
+            throw new Exception($data['msg'] ?: $result->getBody());
+        }
+    }
+
+}
