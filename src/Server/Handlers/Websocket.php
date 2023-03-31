71d83d0ce81e45d64347ad494fbccfd2b3d0d181
duxphp <admin@duxphp.com>
2023-03-31 10:35:40 +0800

    update

--- src/Server/Handlers/Websocket.php
+++ src/Server/Handlers/Websocket.php
@@ -10,7 +10,8 @@ class Websocket
 
     static function start(): void
     {
-        $port = App::config('use')->get('app.port', 8081);
+        $port = App::config('use')->get('app.port', 8080);
+        $port = $port + 1;
         $worker = new Worker("websocket://0.0.0.0:$port");
         $worker->name = 'websocket';
         App::di()->set('ws.worker', $worker);
