3a1f70efa646662dd96741701fe079ce2dab44db
duxphp <admin@duxphp.com>
2023-04-02 15:46:01 +0800

    update queue notify

--- src/View/View.php
+++ src/View/View.php
@@ -6,10 +6,15 @@ namespace Dux\View;
 use Dux\App;
 use Latte\Engine;
 
-class View {
+class View
+{
 
-    public static function init(string $name): Engine {
+    public static function init(string $name): Engine
+    {
         $latte = new Engine;
+        if (!is_dir(App::$dataPath . '/tpl/')) {
+            mkdir(App::$dataPath . '/tpl/', 0777, true);
+        }
         $latte->setTempDirectory(App::$dataPath . '/tpl/' . $name);
         return $latte;
     }
