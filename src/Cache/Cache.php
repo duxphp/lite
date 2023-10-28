79006e642a941c198a89de7d933cdb031089402d
duxweb <admin@dux.plus>
2023-10-28 16:49:33 +0800

    update

--- src/Cache/Cache.php
+++ src/Cache/Cache.php
@@ -4,17 +4,17 @@ declare(strict_types=1);
 namespace Dux\Cache;
 
 use Dux\App;
-use Phpfastcache\CacheManager;
 use Phpfastcache\Config\ConfigurationOption;
 use Phpfastcache\Helper\Psr16Adapter;
 
-class Cache {
+class Cache
+{
 
-    public static function init(string $type, array $config): Psr16Adapter {
+    public static function init(string $type, array $config): Psr16Adapter
+    {
         if ($type === "files") {
             $config["path"] = App::$dataPath . "/cache";
         }
-        CacheManager::setDefaultConfig(new ConfigurationOption($config));
-        return new Psr16Adapter($type);
+        return new Psr16Adapter($type, new ConfigurationOption($config));
     }
 }
